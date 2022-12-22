7e5efd3758a0d97010d32327717066f70c29744d
Juntao Qiu <juntao.qiu@gmail.com>
2022-12-22 17:04:51 +1100

    command + k

--- src/TodoInput.tsx
+++ src/TodoInput.tsx
@@ -1,23 +1,51 @@
-import {TodoType} from "./types";
-import {ChangeEvent, KeyboardEvent, useState} from "react";
-import {v4 as uuid} from "uuid";
+import { TodoType } from "./types";
+import { ChangeEvent, KeyboardEvent as ReactKeyboardEvent, useEffect, useRef, useState } from "react";
+import { v4 as uuid } from "uuid";
 
-export const TodoInput = ({onItemAdded}: { onItemAdded: (todo: TodoType) => void }) => {
-  const [todo, setTodo] = useState<string>('');
+export const TodoInput = ({
+  onItemAdded,
+}: {
+  onItemAdded: (todo: TodoType) => void;
+}) => {
+  const [todo, setTodo] = useState<string>("");
 
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.metaKey && e.key === "k") {
+        if (inputRef.current) {
+          inputRef.current.focus();
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handler);
+
+    return () => {
+      window.removeEventListener("keydown", handler);
+    };
+  });
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setTodo(value);
-  }
+  };
 
-  const handleKeyDown = (e: KeyboardEvent<HTMLElement>) => {
-    if (e.key === 'Enter' && todo.length !== 0) {
+  const handleKeyDown = (e: ReactKeyboardEvent<HTMLElement>) => {
+    if (e.key === "Enter" && todo.length !== 0) {
       const id = uuid();
-      onItemAdded({id: id, content: todo, completed: false})
-      setTodo('');
+      onItemAdded({ id: id, content: todo, completed: false });
+      setTodo("");
     }
-  }
+  };
 
-  return <input type="text" value={todo} onChange={handleChange} onKeyDown={handleKeyDown}
-                placeholder="Buy some milk..."/>
-}
+  return (
+    <input
+      ref={inputRef}
+      type="text"
+      value={todo}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
+      placeholder="⌘ + k to start adding"
+    />
+  );
+};
