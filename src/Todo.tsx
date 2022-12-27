170c091e403ca88f53d1329d071c0f94fbee713c
Juntao Qiu <juntao.qiu@gmail.com>
2022-12-27 11:35:32 +1100

    refactored - extract todoinput

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,14 +1,17 @@
-import {ChangeEvent, KeyboardEvent, useState} from "react";
-import {v4 as uuid} from 'uuid';
+import { ChangeEvent, KeyboardEvent, useState } from "react";
+import { v4 as uuid } from "uuid";
 
 type TodoItem = {
   id: string;
   content: string;
-}
+};
 
-export function Todo() {
-  const [content, setContent] = useState<string>('');
-  const [todos, setTodos] = useState<TodoItem[]>([])
+const TodoInput = ({
+  onItemAdded,
+}: {
+  onItemAdded: (todo: TodoItem) => void;
+}) => {
+  const [content, setContent] = useState<string>("");
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setContent(e.target.value);
@@ -17,23 +20,34 @@ export function Todo() {
   const handleKeyDown = (e: KeyboardEvent<HTMLElement>) => {
     if (e.key === "Enter") {
       const id = uuid();
-      setTodos([{ id, content }, ...todos]);
+      onItemAdded({ id, content });
     }
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        data-testid="todo-input"
-        value={content}
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-      />
+    <input
+      type="text"
+      data-testid="todo-input"
+      value={content}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
+    />
+  );
+};
 
-      {
-        todos.map(todo => <div key={todo.id}>{todo.content}</div>)
-      }
+export function Todo() {
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+
+  const onItemAdded = (todo: TodoItem) => {
+    setTodos([todo, ...todos])
+  }
+
+  return (
+    <div>
+      <TodoInput onItemAdded={onItemAdded} />
+      {todos.map((todo) => (
+        <div key={todo.id}>{todo.content}</div>
+      ))}
     </div>
   );
 }
